39a7efab2568ef838e89c3bdce3289cf43db2e7f
nice-pink <r@nice.pink>
2022-06-10 11:35:27 +0200

    Renaming

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='py-tools',
-    version='0.0.1',
+    name='py_tools',
+    version='0.0.2',
     author='Raffael @ Nice Pink',
     author_email='r@nice.pink',
     description='Python tools',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/nice-pink/py-tools',
+    url='https://github.com/nice-pink/py_tools',
     project_urls = {
-        "Bug Tracker": "https://github.com/nice-pink/py-tools/issues"
+        "Bug Tracker": "https://github.com/nice-pink/py_tools/issues"
     },
     license='MIT',
-    packages=['tools'],
+    packages=['py_tools'],
     install_requires=[],
 )
